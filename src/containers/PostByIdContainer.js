78c41c02c0a3dddbcdb8de93b2dfd180c0cb1439
wondonghwi <ehdehd1289@naver.com>
2021-03-25 10:20:40 +0900

    feat : 리덕스 thunk 최적화

--- src/containers/PostByIdContainer.js
+++ src/containers/PostByIdContainer.js
@@ -1,20 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { clearPost, getPostById } from '../modules/posts';
+import { getPostById } from '../modules/posts';
 import PostById from '../components/PostById';
+import { reducerUtils } from '../lib/asyncUtils';
 
 const PostByIdContainer = ({ postId }) => {
-  const { data, loading, error } = useSelector(state => state.posts.postById);
+  const { data, loading, error } = useSelector(state => state.posts.postById[postId] || reducerUtils.initial());
   const dispatch = useDispatch();
 
   useEffect(() => {
+    if (data) return;
     dispatch(getPostById(postId));
-    return () => {
-      dispatch(clearPost());
-    };
-  }, [dispatch, postId]);
+  }, [data, dispatch, postId]);
 
-  if (loading) return <div>로딩중...</div>;
+  if (loading && !data) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
 
