0ec6c131fca139a747d3e2d6844265a1ad11c4bf
wondonghwi <ehdehd1289@naver.com>
2021-03-25 11:36:12 +0900

    axios -> try catch 추가

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
 export const getPosts = async () => {
-  const response = await axios.get('/posts');
-  return response.data;
+  try {
+    const response = await axios.get('/posts');
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export const getPostById = async id => {
-  const response = await axios.get(`/posts/${id}`);
-  return response.data;
+  try {
+    const response = await axios.get(`/posts/${id}`);
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
 };
